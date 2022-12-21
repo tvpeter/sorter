0ac90dfc39e703e513ae2d9694f7266da9834057
tvpeter <withtvpeter@gmail.com>
2022-12-21 06:14:16 +0100

    add sorting characters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("XanBZE");
+const sortChars = new Sorter(charactersCollection);
+sortChars.sort();
+console.log(charactersCollection.data);
+
+
