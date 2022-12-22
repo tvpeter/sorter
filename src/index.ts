dbede404bc4749293f57033c0866ad4cd83660e1
tvpeter <withtvpeter@gmail.com>
2022-12-22 09:29:12 +0100

    add linklist sorting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -13,3 +14,12 @@ sortChars.sort();
 console.log(charactersCollection.data);
 
 
+const linkList = new LinkList();
+linkList.add(500);
+linkList.add(-10);
+linkList.add(-3);
+linkList.add(4);
+
+const lSorter = new Sorter(linkList);
+lSorter.sort();
+linkList.print();
